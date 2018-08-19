4681bbec2c78bc6157461ccb079dd3f5607b0041
Khalid Hosein <khalid.hosein9@gmail.com>
2018-08-19 11:34:54 -0400

    Able to add expense and income

--- app.js
+++ app.js
@@ -25,7 +25,39 @@ let budgetController = (function() {
             exp: 0,
             inc: 0
         }
-    }
+    };
+
+    return {
+        addItem: function(type, des, val) {
+            let newItem, ID;
+
+            // Create new ID 
+
+            if (data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+
+            // Create new item based on 'inc' or 'exp' type
+            if (type === 'exp') {
+                newItem = new Expense(ID, des, val);
+            } else if (type === 'inc') {
+                newItem = new Income(ID, des, val);
+            }
+
+
+            // Push it into data structure
+            data.allItems[type].push(newItem)
+
+            // Return the new elements
+            return newItem;
+        },
+
+        testing: function() {
+            console.log(data);
+        }
+    };
 
 })();
 
@@ -46,7 +78,7 @@ let UIController = (function() {
         getInput: function() {
             return {
 
-                type: document.querySelector(DOMstrings.inputType).value, // Will be either Income or Expense
+                type: document.querySelector(DOMstrings.inputType).value, // Will be either Inc or Exp
                 description: document.querySelector(DOMstrings.inputDescription).value,
                 value: document.querySelector(DOMstrings.inputValue).value
 
@@ -81,13 +113,13 @@ let controller = (function(budgetCtrl, UICtrl) {
 
 
     let ctrlAddItem = function() {
+        let input, newItem;
 
         // 1. Get the field input data
-        let input = UICtrl.getInput();
-
-
+        input = UICtrl.getInput();
 
         // 2. Add item to budget controller
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
         // 3. Add the item to user interface
 
